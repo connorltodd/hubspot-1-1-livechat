482839d562cfc044cd82fa572ead51b7ce38fb80
Connor Todd <connortodd0304@gmail.com>
2021-01-15 12:10:40 +0000

    https://github.com/connorltodd/hubspot-1-1-livechat

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <script
+        type="text/javascript"
+        id="hs-script-loader"
+        async
+        defer
+        src="//js.hs-scripts.com/9027807.js"
+      ></script>
+    </div>
+  );
+}
+
+export default App;
